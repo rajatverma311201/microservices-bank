4e7f1b32f88345edaca2b86889694b396b01af8e
Rajat Verma <rajat311201@gmail.com>
2025-06-27 18:59:14 +0530

    basic cards setup

---
+++ cards/src/test/java/in/rajat_verma/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rajat_verma.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
