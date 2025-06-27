4bea3b7f25ad8b7af0b6061b31f88cc2c772b989
Rajat Verma <rajat311201@gmail.com>
2025-06-27 18:59:35 +0530

    basic loans setup

---
+++ loans/src/main/java/in/rajat_verma/loans/mapper/LoanMapper.java
@@ -0,0 +1,29 @@
+package in.rajat_verma.loans.mapper;
+
+
+import in.rajat_verma.loans.dto.LoanDto;
+import in.rajat_verma.loans.entity.Loan;
+
+public class LoanMapper {
+
+    public static LoanDto mapToLoanDto(Loan loan, LoanDto loanDto) {
+        loanDto.setLoanNumber(loan.getLoanNumber());
+        loanDto.setLoanType(loan.getLoanType());
+        loanDto.setMobileNumber(loan.getMobileNumber());
+        loanDto.setTotalLoan(loan.getTotalLoan());
+        loanDto.setAmountPaid(loan.getAmountPaid());
+        loanDto.setOutstandingAmount(loan.getOutstandingAmount());
+        return loanDto;
+    }
+
+    public static Loan mapToLoan(LoanDto loanDto, Loan loan) {
+        loan.setLoanNumber(loanDto.getLoanNumber());
+        loan.setLoanType(loanDto.getLoanType());
+        loan.setMobileNumber(loanDto.getMobileNumber());
+        loan.setTotalLoan(loanDto.getTotalLoan());
+        loan.setAmountPaid(loanDto.getAmountPaid());
+        loan.setOutstandingAmount(loanDto.getOutstandingAmount());
+        return loan;
+    }
+
+}
---
+++ loans/src/test/java/in/rajat_verma/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package in.rajat_verma.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
